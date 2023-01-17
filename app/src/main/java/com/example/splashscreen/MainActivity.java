a9f96833bb0292022ad387ea80ebaa25c1fc8d58
Suryansh Jaiswal <suryanshnanu@gmail.com>
2023-01-17 20:25:52 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/splashscreen/MainActivity.java
+++ app/src/main/java/com/example/splashscreen/MainActivity.java
@@ -1,11 +1,8 @@
 package com.example.splashscreen;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
